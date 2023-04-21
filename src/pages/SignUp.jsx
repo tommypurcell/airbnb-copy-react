fbb82836751fa35b90db8bf4b26bc555b181d5ea
tommypurcell <120073087+tommypurcell@users.noreply.github.com>
2023-04-22 01:31:49 +0700

    SignUp.js now creates object
    When form is submitted a function is called that returns an object with all of the data from the form.

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,4 +1,17 @@
 export default function SignUp() {
+  // functions
+
+  // function to save signup form into object
+  function signUp(e) {
+    e.preventDefault()
+
+    let obj = {}
+    obj.name = e.target.fullName.value
+    obj.profilePicture = e.target.profilePicture.value
+    obj.email = e.target.email.value
+    obj.password = e.target.password.value
+    console.log(obj)
+  }
   return (
     <div className="card">
       <img src="images/thailand.jpg" className="card-img" alt="..." />
@@ -10,32 +23,39 @@ export default function SignUp() {
             className="d-block w-25 pt-5 pb-5"
           />
           <div className="card-body container border">
-            <form>
+            <form onSubmit={(e) => signUp(e)}>
               <div className="row text-start">Your Full Name</div>
               <div className="row">
-                <input type="text" className="border rounded" />
+                <input type="text" name="fullName" className="border rounded" />
               </div>
               <div className="row text-start">Profile Picture</div>
               <div className="row">
-                <input type="url" className="border rounded" />
+                <input
+                  type="url"
+                  name="profilePicture"
+                  className="border rounded"
+                />
               </div>
               <div className="row text-start">Email</div>
               <div className="row">
-                <input type="email" className="border rounded" />
+                <input type="email" name="email" className="border rounded" />
               </div>
               <div className="row text-start">Password</div>
               <div className="row">
-                <input type="password" className="border rounded" />
+                <input
+                  type="password"
+                  name="password"
+                  className="border rounded"
+                />
               </div>
-              <button type="button" className="btn btn-success row mt-3">
+              <button type="submit" className="btn btn-success row mt-3">
                 Sign Up
               </button>
-              <div className="row">
-                <span>
-                  Already have an account? <a href="">Login</a>
-                </span>
-              </div>
+              <div className="row"></div>
             </form>
+            <span>
+              Already have an account? <a href="">Login</a>
+            </span>
           </div>
         </div>
       </div>
